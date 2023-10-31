70409b954595bcd0439373a0969a0eba837251e3
pedro-arp <pedroagtrodrigues@gmail.com>
2023-10-30 22:48:57 -0300

    Concorrência - Executors - Interface Callable | Future. CompletableFuture - get and join | Streams | ThreadFactory | Encadeando chamadas | allOf & allAny

---
+++ devdojo/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/service/StoreServiceDeprecated.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.service;
+
+import java.util.concurrent.*;
+
+public class StoreServiceDeprecated {
+    public double getPriceSync(String storeName) {
+        System.out.printf("Getting prices sync for store %s %n", storeName);
+        return priceGenerator();
+    }
+    private double priceGenerator() {
+        System.out.printf("%s generating price %n", Thread.currentThread().getName());
+        delay();
+        return ThreadLocalRandom.current().nextDouble(1, 500) * 10;
+    }
+
+    private void delay() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
