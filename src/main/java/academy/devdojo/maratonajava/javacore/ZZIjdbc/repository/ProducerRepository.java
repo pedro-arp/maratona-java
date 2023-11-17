581df9f8a28330d8a874345eaa07f2c629fdf8ce
Pedro Augusto <pedroagtrodrigues@gmail.com>
2023-11-17 12:31:27 -0300

    Inserting information in Database - Save, delete and update

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -14,13 +14,34 @@ public class ProducerRepository {
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s')".formatted(producer.getName());
         try (Connection conn = ConnectionFactory.getConnection(); Statement stmt = conn.createStatement()) {
             final int rowsAffected = stmt.executeUpdate(sql);
-            log.info("Inserted producer in the database, rows affected '{}'", rowsAffected);
+            log.info("Inserted producer '{}' in the database, rows affected '{}'",producer.getName(), rowsAffected);
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            log.error("Error while trying to insert producer '{}'", producer.getName(), e);
         }
 
 
+    }
+    public static void delete(int id) {
+        String sql = "DELETE FROM `anime_store`.`producer` WHERE (`id` = '%d');".formatted(id);
+        try (Connection conn = ConnectionFactory.getConnection(); Statement stmt = conn.createStatement()) {
+            final int rowsAffected = stmt.executeUpdate(sql);
+            log.info("Deleted producer '{}' from the database, rows affected '{}'",id, rowsAffected);
+
+        } catch (SQLException e) {
+            log.error("Error while trying to delete producer '{}'", id, e);
+        }
+}
+    public static void update(Producer producer) {
+        String sql = "UPDATE `anime_store`.`producer` SET `name` = '%s' WHERE (`id` = '%d');"
+                .formatted(producer.getName(), producer.getId());
+        try (Connection conn = ConnectionFactory.getConnection(); Statement stmt = conn.createStatement()) {
+            final int rowsAffected = stmt.executeUpdate(sql);
+            log.info("Updated producer '{}' , rows affected '{}'",producer.getId(), rowsAffected);
+
+        } catch (SQLException e) {
+            log.error("Error while trying to delete producer '{}'", producer.getId(), e);
+        }
     }
 }
 
